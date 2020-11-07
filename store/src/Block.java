325e24026f87742e8d01d59792c0608f7c8af1b2
Kamalov Bulat <mcrazerman@gmail.com>
2020-11-07 10:55:14 +0300

    Update

--- store/src/Block.java
+++ store/src/Block.java
@@ -1,7 +1,9 @@
 import java.util.List;
 import java.util.Map;
 
-public class Block {
+public class Block
+{
+    //
     public String prev_hash;
     public int time;
     public List<Integer> mining_info;
